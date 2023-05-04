a91f5c0a447cea1563dfd1f70573f4b25953237c
Daniel Du <danieldu109@gmail.com>
2023-05-04 01:23:40 +0100

    pom.xml para junit e mvn funfagadd

--- POO_Project/src/test/java/CasaInteligenteTest.java
+++ POO_Project/src/test/java/CasaInteligenteTest.java
@@ -1,13 +1,10 @@
 package src.test.java;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import src.main.java.*;
 
-import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Test;
 
 
 class CasaInteligenteTest {
